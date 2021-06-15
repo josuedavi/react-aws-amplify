81fcaab15b973449d968b5c5f1c12d2561e5fce0
Josue Castillo <30334352+josuedavi@users.noreply.github.com>
2021-06-15 12:32:26 -0400

    authentication

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,10 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World! (AWS)</h1>
+      <h1>Hello, AWS!</h1>
+       <header>
+        <h1>We now have Auth!</h1>
+      </header>
       <AmplifySignOut />
     </div>
   );
